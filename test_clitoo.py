9c25e41581d4d20e8f0670772b9cbb690a061740
jpic <jpic@yourlabs.org>
2018-11-18 05:13:42 +0100

    qa

--- test_clitoo.py
+++ test_clitoo.py
@@ -5,23 +5,23 @@ class Foo:
     bar = {'baz': [lambda *a, **k: 'test']}
 
 
-def test_Callback_nested_attribute():
+def test_callback_nested_attribute():
     cb = clitoo.Callback.factory('test_clitoo.Foo.bar.baz.0')
     assert cb.cb == Foo.bar['baz'][0]
 
 
-def test_Callback_nested_module():
+def test_callback_nested_module():
     from clitoo.git import clone
     cb = clitoo.Callback.factory('clitoo.git.clone')
     assert cb.cb == clone
 
 
-def test_Callback_fail():
+def test_callback_fail():
     cb = clitoo.Callback.factory('clitoooooooolaaaawwwwllllzz')
     assert cb.cb is None
 
 
-def test_Callback_callables():
+def test_callback_callables():
     cb = clitoo.Callback.factory('clitoo')
     assert 'help' in cb.callables
 
