be138833a5f1a7f26a5b206993d3b47df8187552
jpic <jpic@yourlabs.org>
2019-01-22 16:15:58 +0100

    Bugfix in Cli2ArgsException

--- cli2/exceptions.py
+++ cli2/exceptions.py
@@ -9,8 +9,11 @@ class Cli2ArgsException(Cli2Exception):
     def __init__(self, command, passed_args):
         msg = ['Got arguments:'] if len(passed_args) else []
 
-        for arg, i in enumerate(passed_args, start=0):
-            msg.append('='.join(command.required_args[i], arg))
+        for i, arg in enumerate(passed_args, start=0):
+            msg.append('='.join([command.required_args[i], arg]))
+
+        if msg:
+            msg.append('')
 
         msg.append(
             'Missing arguments: ' + ', '.join([*map(
