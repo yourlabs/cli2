c1f1fa6817d8799a57ba39e250ab56a311c37147
jpic <jpic@yourlabs.org>
2020-05-03 13:44:57 +0200

    Make help a proper command
    - recursive Group.load()
    - preventing from importing stuff from above level
    - preventing recursive same-level imports
    - rely purely on the help command in cli2.cli

--- cli2/group.py
+++ cli2/group.py
@@ -27,6 +27,9 @@ class Group(EntryPoint, dict):
         self.color = color or colors.green
         self.posix = posix
 
+        # make help a group command
+        self.cmd(self.help)
+
     def add(self, target, *args, **kwargs):
         cmd = Command(target, *args, **kwargs)
         self[cmd.name] = cmd
@@ -53,15 +56,34 @@ class Group(EntryPoint, dict):
     def arg(self, name, **kwargs):
         return arg(name, **kwargs)
 
-    def help(self, error=None, short=False):
+    def group(self, name, **kwargs):
+        self[name] = Group(name, **kwargs)
+        return self[name]
+
+    def help(self, *args, error=None, short=False):
+        """Get help for a command or group."""
+        if args:
+            target = self
+            for arg in args:
+                if arg in target:
+                    target = target[arg]
+                elif isinstance(target, Command):
+                    return target.help(error=error)
+                else:
+                    error = f'Command {arg} not found in {target}'
+                    error += f'Showing help for {target}'
+                    break
+            return target.help(error=error)
+
         output = []
+
         if error:
             output.append(error + '\n')
 
         if self.doc:
             output.append(self.doc + '\n')
 
-        if not len(self):
+        if not len(self) or short:
             return '\n'.join(output)
 
         namewidth = 2 + max([len(key) for key in self]) + 2
@@ -84,12 +106,15 @@ class Group(EntryPoint, dict):
                 '  ',
                 out,
             ]
-            output.append(''.join(map(str, line)))
+            output.append((''.join(map(str, line))).rstrip())
         return '\n'.join(output)
+    help.cli2 = dict(color='green')
 
-    def load(self, obj):
+    def load(self, obj, parent=None):
         if isinstance(obj, str):
             obj = Node.factory(obj).target
+
+        objpackage = getattr(obj, '__package__', None)
 
         for name in dir(obj):
             if name == '__call__':
@@ -99,34 +124,41 @@ class Group(EntryPoint, dict):
             else:
                 target = getattr(obj, name)
 
-            if callable(target):
-                cmd = Command(target)
-                self[cmd.name] = cmd
+            targetpackage = getattr(target, '__package__', None)
+            if targetpackage and objpackage:
+                # prevent recursively loading from other packages
+                # and above obj level
+                if not targetpackage.startswith(objpackage):
+                    continue
+
+            if target == parent:
+                # detect and prevent recursive imports
                 continue
-            else:
-                self[name] = Group(name)
+
+            if callable(target):
+                try:
+                    inspect.signature(target)
+                except ValueError:
+                    pass
+                else:
+                    self.add(target)
+                continue
+
+            node = Node(name, target)
+            if node.callables:
+                self.group(name).load(target, parent=obj)
 
     def __call__(self, *argv):
         self.exit_code = 0
         if not argv:
-            return self.help('No command provided, showing help.')
+            return self.help(error='No sub-command provided')
 
         if argv[0] in self:
             result = self[argv[0]](*argv[1:])
             # fetch exit code
             self.exit_code = self[argv[0]].exit_code
-        elif argv[0] == 'help':
-            if len(argv) > 1:
-                if argv[1] in self:
-                    return self[argv[1]].help()
-                else:
-                    return self.help(
-                        f'Command {argv[0]} not found, showing help.'
-                    )
-            else:
-                return self.help()
         else:
-            return self.help(f'Command {argv[0]} not found, showing help.')
+            return self.help(error=f'Command {argv[0]} not found')
 
         return result
 
