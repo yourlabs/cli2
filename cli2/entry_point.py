b4b49d6f6fdace095c108fbd36b00410d1f3ca6b
jpic <jpic@yourlabs.org>
2020-05-04 12:14:07 +0200

    entry_point: set self.name, and exit with self.exit_code

--- cli2/entry_point.py
+++ cli2/entry_point.py
@@ -1,10 +1,13 @@
+import os
 import sys
 
 
 class EntryPoint:
     def entry_point(self):
         self.exit_code = 0
+        if not self.name:
+            self.name = os.path.basename(sys.argv[0])
         result = self(*sys.argv[1:])
         if result is not None:
             print(result)
-        return self.exit_code
+        sys.exit(self.exit_code)
