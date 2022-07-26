6848a51c350245af31c1c6b1e73c1849a33d31de
jpic <jpic@yourlabs.org>
2022-07-26 11:44:02 +0200

    Activate Python 3.6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     license='MIT',
     keywords='cli',
-    python_requires='>=3.8',
+    python_requires='>=3.6',
     entry_points={
         'console_scripts': [
             'cli2 = cli2.cli:main.entry_point',
