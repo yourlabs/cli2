d50a6b3ba7486db4f7dc8eacecb80e96629df158
jpic <jpic@yourlabs.org>
2025-02-14 16:40:11 +0100

    Client request command

--- cli2/example_client.py
+++ cli2/example_client.py
@@ -2,12 +2,18 @@ import cli2
 
 
 class APIClient(cli2.Client):
+    """
+    Client for restful-api.dev
+    """
     def __init__(self, *args, **kwargs):
         kwargs.setdefault('base_url', 'https://api.restful-api.dev/')
         super().__init__(*args, **kwargs)
 
 
 class Object(APIClient.Model):
+    """
+    restful-api.dev objects
+    """
     url_list = '/objects'
     url_detail = '/objects/{self.id}'
 
@@ -16,12 +22,14 @@ class Object(APIClient.Model):
     @cli2.cmd
     @classmethod
     async def fail(cls):
-        await cls.client.post('/foo', json=[1])
+        """ Send bogus JSON """
+        await cls.client.post('/foo', json=dict(a=1))
 
     @classmethod
     @cli2.cmd
     async def fail2(cls):
-        await cls.client.post('/foo', json=[1])
+        """ Send bogus Form data """
+        await cls.client.post('/foo', data=dict(b=2))
 
 
 cli = APIClient.cli
