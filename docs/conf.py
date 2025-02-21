a6937b7688fa213a750ec5ea218464bc9a18f2a5
jpic <jpic@yourlabs.org>
2025-02-21 14:40:05 +0100

    3.4 tag

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019-2020, YourLabs'
 author = 'jpic, jdkirkwood'
 
 # The short X.Y version
-version = '3.1'
+version = '3.4'
 # The full version, including alpha/beta/rc tags
-release = '3.1.0'
+release = '3.4.0'
 
 
 # -- General configuration ---------------------------------------------------
