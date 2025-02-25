51c2e321699a3413782561d7389ba761022e74b4
jpic <jpic@yourlabs.org>
2025-02-25 18:25:39 +0100

    Don't try to import ansible outside of playbook fixture to not break pytest

--- cli2/ansible/pytest.py
+++ cli2/ansible/pytest.py
@@ -1,10 +1,7 @@
 import pytest
 
-try:
+
+@pytest.fixture
+def playbook(tmp_path, request):
     from .playbook import Playbook
-except ImportError:
-    pass
-else:
-    @pytest.fixture
-    def playbook(tmp_path, request):
-        return Playbook(tmp_path, name=request.node.originalname)
+    return Playbook(tmp_path, name=request.node.originalname)
