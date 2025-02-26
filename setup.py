c394f50d2855f27f05cec4d44c15f8cd3ec9ec68
jpic <jpic@yourlabs.org>
2025-02-26 17:54:19 +0100

    Added setup.py hacks for maybe one day

--- setup.py
+++ setup.py
@@ -1,5 +1,46 @@
 from setuptools import setup
 
+# We're not having that for the moment
+# from setuptools.command.install import install
+# from setuptools.command.develop import develop
+# from pathlib import Path
+# import os
+# import shutil
+# import sys
+#
+#
+# class CollectionInstaller:
+#     @property
+#     def collection_target(self):
+#         target = Path(os.getenv('HOME'))
+#         target /= '.ansible/collections/ansible_collections/yourlabs/cli2'
+#         return target
+#
+#
+# class Develop(CollectionInstaller, develop):
+#     def run(self):
+#         super().run()
+#
+#         source = Path(self.egg_path) / 'cli2/ansible/collection'
+#         if self.collection_target.exists():
+#             if self.collection_target.is_symlink():
+#                 if self.collection_target.resolve() == source:
+#                     return
+#             elif self.collection_target.is_dir():
+#                 shutil.rmtree(self.collection_target)
+#
+#         self.collection_target.parent.mkdir(exist_ok=True, parents=True)
+#         self.collection_target.symlink_to(source)
+#
+#
+# class Install(CollectionInstaller, install):
+#     def run(self):
+#         super().run()
+#
+#         self.collection_target.parent.mkdir(exist_ok=True, parents=True)
+#         source = Path(self.install_lib) / 'cli2/ansible/collection'
+#         shutil.copytree(source, self.collection_target, dirs_exist_ok=True)
+
 
 setup(
     name='cli2',
@@ -42,4 +83,8 @@ setup(
             'cli2-example-client = cli2.examples.client:cli.entry_point',
         ],
     },
+    # cmdclass={
+    #     'install': Install,
+    #     'develop': Develop,
+    # },
 )
