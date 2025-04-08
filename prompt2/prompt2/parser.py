95370ca92096be2b228ea2317afae532c0736840
jpic <jpic@yourlabs.org>
2025-04-08 09:02:54 +0200

    prompt2: list parser

--- prompt2/prompt2/parser.py
+++ prompt2/prompt2/parser.py
@@ -6,6 +6,7 @@ from .exception import NotFoundError
 
 class Parser:
     entry_point = 'prompt2_parser'
+    system = None
 
     class NotFoundError(NotFoundError):
         title = 'PARSER NOT FOUND'
@@ -30,6 +31,16 @@ class Parser:
         return [*plugins][0].load()
 
     def messages(self, messages):
+        if self.system:
+            messages.append(
+                dict(
+                    role='system',
+                    content=self.system,
+                ),
+            )
+        return messages
+
+    def messages(self, messages):
         return messages
 
     def parse(self, response):
@@ -39,17 +50,16 @@ class Parser:
 class Wholefile(Parser):
     system = "Respond only with the complete new file content, as this will be directly written to the file by an automated AI assistant tool—no additional text, comments, or explanations are allowed."  # noqa
 
-    def messages(self, messages):
-        messages.append(
-            dict(
-                role='system',
-                content=self.system,
-            ),
-        )
-        return messages
-
     def parse(self, response):
         if response.startswith('```'):
             # strip markup the IA absolutely wants to add
             return '\n'.join([l for l in response.split('\n')[1:-1]])
+        return response
+
+
+class List(Parser):
+    system = "Respond only with the requested list because it will be processed by an automated AI assistant tool—no additional text, comments, or explanations are allowed."  # noqa
+
+    def parse(self, response):
+        breakpoint()
         return response
